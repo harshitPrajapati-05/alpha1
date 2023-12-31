441ddb1d350f8882afd1f2b54706162857554858
Harshit Prajapati <harshitprajapati5114@gmail.com>
2023-12-31 11:26:34 +0530

    alpka

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,34 @@
-import conf from "../conf"
 
-const Header = ({}) => {
+import {NavLink} from 'react-router-dom'
+const Header = () => {
   return (
     <>
-   
-
+    <div className="flex justify-between  h-14 border-[0.9px] focus-within:border-white hover:border-white  items-center" >
+      <ul className='text-white flex flex-wrap gap-16 ml-[6rem]'>
+        <li>
+          <NavLink to='/'
+          className={({isActive})=>`${isActive?  "text-blue-200":"text-white line-through  hover:no-underline"}`}>
+            HOME
+          </NavLink>
+        </li>
+        <li>
+        <NavLink to='/user'
+          className={({isActive})=>`${isActive?  "text-blue-200 ":"text-white line-through hover:no-underline"}`}>
+            USERS
+          </NavLink>
+        </li>
+        <li>
+        <NavLink to='/pics'
+          className={({isActive})=>`${isActive?  "text-blue-200":"text-white line-through hover:no-underline "}`}>
+            PICS 
+          </NavLink>
+        </li>
+      </ul>
+      <div className='text-black  m-2 '>
+        <NavLink  to='/signup'className={({isActive})=>` ${isActive?"text-white":"text-blue-200"} px-1 py-2  border-[1.2px]  font-mono font-medium rounded-l-lg`} >Get Started</NavLink>
+        <NavLink to='/login' className={({isActive})=>` ${isActive?"text-white":"text-blue-200"} px-1 py-2  border-[1.2px]  font-mono font-medium rounded-r-lg`}>Let's Continue</NavLink>
+      </div>
+    </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements,Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Users from './pages/Users.jsx'
+import Pics from './Pics.jsx'
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Home/>}>
+      <Route path='/user' element={<Users/>}/>
+      <Route path='/pics' element={<Pics/>}/>
+      <Route path='/Signup' />
+      <Route path='/login'/>
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode> 
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
